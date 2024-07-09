fd0772f78aee6e68ec0b64754ab78df8b2a0e2ca
SamLieuwen <samlieuwen12@gmail.com>
2024-07-09 16:43:34 -0500

    Turn Order

--- Go Fish/GoFish.cs
+++ Go Fish/GoFish.cs
@@ -11,18 +11,82 @@ namespace GoFish
         public static List<Card> dH;
         public static int pS;
         public static int dS;
+        public static bool runGame;
 
         static void Main(string[] args)
         {
             GoFish game = new GoFish();
+            Random rnd = new Random();
+            string guess;
+            int coin;
+            bool firstTurn = false;
 
             deck = new List<Card>();
             pH = new List<Card>();
             dH = new List<Card>();
             pS = 0;
             dS = 0;
+            runGame = true;
 
             GoFish.createDeck();
+
+            for (int i = 0; i < 7; i++)
+            {
+                pH.Add(deck[0]);
+                deck.RemoveAt(0);
+                dH.Add(deck[0]);
+                deck.RemoveAt(0);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Welcome to Go Fish!");
+
+            while (true)
+            {
+                Console.WriteLine("Heads or Tails: ");
+                guess = Console.ReadLine().ToLower();
+                coin = rnd.Next(1, 3);
+
+                if (guess != "heads" && guess != "tails")
+                {
+                    Console.WriteLine("Invalid Response");
+                }
+                else { break; }
+            }
+
+            if (guess == "heads" && coin == 1 || guess == "tails" && coin == 2)
+            {
+                firstTurn = true;
+                Console.WriteLine("\nYou go first\nPress any key to continue");
+            }
+            else { Console.WriteLine("\nComputer goes first\nPress any key to continue"); }
+            Console.ReadKey();
+
+            if (firstTurn == true)
+            {
+                while (runGame)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Your Hand: ");
+                    foreach (Card card in pH)
+                    {
+                        Console.Write(card.card + " ");
+                    }
+                }
+            }
+            else
+            {
+                while (runGame)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Your Hand: ");
+                    foreach (Card card in pH)
+                    {
+                        Console.Write(card.card + " ");
+                    }
+                }
+            }
+            
         }
 
         public static void createDeck()
