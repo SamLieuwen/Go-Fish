0c713deb329290ad2edf9619b55df3c9b4871196
SamLieuwen <samlieuwen12@gmail.com>
2024-07-18 08:12:08 -0500

    More formatting

--- Go Fish/GoFish.cs
+++ Go Fish/GoFish.cs
@@ -72,7 +72,7 @@ namespace GoFish
 
                     if (Decks.playerHand.Count() > 0)
                     {
-                        Actions();
+                        Actions.PlayerActions();
                         playerScore = Decks.Pairs(Decks.playerHand, playerScore);
                     }
                     if (Decks.playerHand.Count() == 0)
@@ -85,7 +85,7 @@ namespace GoFish
 
                     if (Decks.dealerHand.Count() > 0)
                     {
-                        DealerActions();
+                        Actions.DealerActions();
                         dealerScore = Decks.Pairs(Decks.dealerHand, dealerScore);
                     }
                     if (Decks.dealerHand.Count() == 0)
@@ -109,7 +109,7 @@ namespace GoFish
 
                     if (Decks.dealerHand.Count() > 0)
                     {
-                        DealerActions();
+                        Actions.DealerActions();
                         dealerScore = Decks.Pairs(Decks.dealerHand, dealerScore);
                     }
                     if (Decks.dealerHand.Count() == 0)
@@ -122,7 +122,7 @@ namespace GoFish
 
                     if (Decks.playerHand.Count() > 0)
                     {
-                        Actions();
+                        Actions.PlayerActions();
                         playerScore = Decks.Pairs(Decks.playerHand, playerScore);
                     }
                     if (Decks.playerHand.Count() == 0)
@@ -137,127 +137,6 @@ namespace GoFish
                         Results();
                     }
                 }
-            }
-        }
-
-        public static void Actions()
-        {
-            Card temp = new Card(null);
-            int dealerCount = Decks.dealerHand.Count();
-            bool action = true;
-            bool error = true;
-            bool check = false;
-            bool inHand = false;
-            bool indealerHandand = false;
-
-            while (action)
-            {
-                while (error)
-                {
-                    try
-                    {
-                        Console.WriteLine("\nWhich card would you like to ask for?");
-                        temp.card = Console.ReadLine().ToLower();
-                        temp.card = char.ToUpper(temp.card[0]) + temp.card.Substring(1);
-
-                        foreach (Card card in Decks.referenceDeck)
-                        {
-                            if (card.card == temp.card)
-                            {
-                                check = true;
-                                error = false;
-                                break;
-                            }
-                        }
-                        if (check == false)
-                        {
-                            Console.WriteLine("Invalid Response");
-                        }
-                    }
-                    catch (Exception) { Console.WriteLine("Invalid Response"); }
-                }
-
-                foreach (Card card in Decks.playerHand)
-                {
-                    if (card.card == temp.card)
-                    {
-                        temp = card;
-                        inHand = true;
-                        break;
-                    }
-                }
-
-                if (inHand == true)
-                {
-                    foreach (Card card in Decks.dealerHand)
-                    {
-                        if (card.card == temp.card)
-                        {
-                            action = false;
-                            indealerHandand = true;
-                            temp = card;
-                            break;
-                        }
-                    }
-                    action = false;
-                }
-                else { Console.WriteLine("\nYour hand doesn't contain a " + temp.card); error = true; }
-            }
-            if (indealerHandand == true)
-            {
-                Console.WriteLine("\nYou took the computer's card\nPress any key to continue");
-                Console.ReadKey();
-
-                Decks.playerHand.Add(temp);
-                Decks.dealerHand.Remove(temp);
-            }
-            else
-            {
-                Console.WriteLine("Go Fish!\nYou picked up a: " + Decks.deck[0].card + "\n\nPress any key to continue");
-                Console.ReadKey();
-
-                Decks.playerHand.Add(Decks.deck[0]);
-                Decks.deck.RemoveAt(0);
-            }
-        }
-        public static void DealerActions()
-        {
-            Random rnd = new Random();
-            Card temp = new Card(null);
-            int dealerHandCount = Decks.dealerHand.Count();
-            int index = 0;
-            bool inHand = false;
-
-            index = rnd.Next(0, dealerHandCount);
-
-            foreach (Card card in Decks.playerHand)
-            {
-                if (Decks.dealerHand[index].card == card.card)
-                {
-                    inHand = true;
-                    temp = card;
-                }
-            }
-
-            if (inHand == true)
-            {
-                Console.WriteLine("\nDo you have a " + Decks.dealerHand[index].card);
-                Console.WriteLine("\nThe computer took your card\nPress any key to continue");
-
-                Decks.dealerHand.Add(temp);
-                Decks.playerHand.Remove(temp);
-
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("\nDo you have a " + Decks.dealerHand[index].card);
-                Console.WriteLine("Go Fish!\nPress any key to continue");
-
-                Decks.dealerHand.Add(Decks.deck[0]);
-                Decks.deck.RemoveAt(0);
-
-                Console.ReadKey();
             }
         }
 
